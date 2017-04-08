455f3770fba75d1b8478f2ccf8aad4ff7a703c8e
Mark Davidson <marko.davo@gmail.com>
2017-04-08 13:54:06 +1000

    Add project files.

---
+++ Stack/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+        }
+    }
+}
---
+++ Stack/Stack.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+
+namespace StackApp
+{
+    public class Stack : Object
+    {
+        private object object1;
+        private readonly ArrayList list = new ArrayList();
+
+        public void Push(object obj)
+        {
+            if (obj != null)
+            {
+                list.Add(obj); 
+            }
+            else
+            {
+                throw new InvalidOperationException("You can not push nothing to the Stack"); 
+            }
+        }
+
+        public object Pop()
+        {
+            if (list.Count != 0)
+            {
+                object1 = list[list.Count - 1];
+                list.Remove(object1); 
+            } else
+            {
+                throw new InvalidOperationException("Stack is already empty. Nothing to remove. ");
+            }
+            return object1; 
+        }
+
+        public void Clear()
+        {
+            if (list.Count != 0)
+            {
+                list.Clear(); 
+            }
+            else
+            {
+                throw new InvalidOperationException("Stack is already cleared");
+            }
+        }
+    }
+}
